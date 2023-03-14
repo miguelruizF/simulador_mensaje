b9bffacad17ef7492919374d36861cc61911cd84
Miguel Ruiz <later481@gmail.com>
2023-03-13 22:24:43 -0600

    seleccion de inputs

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const d = document;
+
+d.addEventListener("DOMContentLoaded", ()=> {
+    //Seleccionar elementos
+    const inputEmail = d.querySelector("#email");
+    const inputAsunto = d.querySelector("#asunto");
+    const inputMensaje = d.querySelector("#mensaje");
+    console.log(inputMensaje);
+});
