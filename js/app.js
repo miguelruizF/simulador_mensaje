4ca0ff36f17e816d666cd5f11cc852e177584a9b
Miguel Ruiz <later481@gmail.com>
2023-03-14 21:12:44 -0600

    codigo de inputs errores

--- js/app.js
+++ js/app.js
@@ -5,5 +5,29 @@ d.addEventListener("DOMContentLoaded", ()=> {
     const inputEmail = d.querySelector("#email");
     const inputAsunto = d.querySelector("#asunto");
     const inputMensaje = d.querySelector("#mensaje");
-    console.log(inputMensaje);
+    const form = d.querySelector("form");
+    // console.log(inputMensaje);
+
+    //Asignar eventos
+    inputEmail.addEventListener("blur",validar);
+    inputAsunto.addEventListener("blur", validar);
+    inputMensaje.addEventListener("blur", validar);
+
+    function validar(e) {
+        if(e.target.value.trim() === ""){
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
+            // console.log("Esta vacio");
+        }else{
+            // console.log("No esta vacio");
+        }
+    }
+
+    function mostrarAlerta(value){
+        const error = d.createElement("p");
+        error.classList.add("bg-red-600","text-white","p-2","text-center");
+        error.textContent = value;
+
+        //Inyectar el error
+        form.appendChild(error);
+    }
 });
