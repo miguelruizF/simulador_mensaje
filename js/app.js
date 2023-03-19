66c1226e9e0506f66d80b26e74dd7682934a5122
Miguel Ruiz <later481@gmail.com>
2023-03-18 20:25:17 -0600

    funciones habilitar submit

--- js/app.js
+++ js/app.js
@@ -6,24 +6,42 @@ d.addEventListener("DOMContentLoaded", ()=> {
     const inputAsunto = d.querySelector("#asunto");
     const inputMensaje = d.querySelector("#mensaje");
     const form = d.querySelector("form");
+    const btnSubmit = d.querySelector("#formulario button[type='submit']")
+
+    const email = {
+        email: "",
+        asunto: "",
+        mensaje: "",
+    }
     // console.log(inputMensaje);
 
     //Asignar eventos
-    inputEmail.addEventListener("blur",validar);
-    inputAsunto.addEventListener("blur", validar);
-    inputMensaje.addEventListener("blur", validar);
+    inputEmail.addEventListener("input",validar);
+    inputAsunto.addEventListener("input", validar);
+    inputMensaje.addEventListener("input", validar);
 
     function validar(e) {
         if(e.target.value.trim() === ""){
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
             // console.log("Esta vacio");
+            email[e.target.name] = "";
+            comprobarEmail();
             return;
         }
         if(e.target.id === "email" && !validarEmail(e.target.value)){
             mostrarAlerta(`El ${e.target.id} no es valido`, e.target.parentElement);
+            email[e.target.name] = "";
+            comprobarEmail();
             return;
         }
         limpiarAlerta(e.target.parentElement);
+
+        //Asignar valores
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+        // console.log(email);
+        
+        //Comprobar el objeto de email
+        comprobarEmail();
     }
 
     function mostrarAlerta(value, referencia){
@@ -53,5 +71,16 @@ d.addEventListener("DOMContentLoaded", ()=> {
         const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
         const resultado = regex.test(email);
         return resultado;
+    }
+
+    function comprobarEmail(){
+        // console.log(email);
+        if(Object.values(email).includes("")){
+            btnSubmit.classList.add("opacity-50");
+            btnSubmit.disable = true;
+            return;
+        }
+        btnSubmit.classList.remove("opacity-50");
+        btnSubmit.disable = false;
     }
 });
